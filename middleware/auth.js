21065c453dc0af2ced58abf21e2fe1b389bdab91
samueloguta <ogutasamuel367@gmail.com>
2025-07-20 16:10:12 +0300

    Add complete auth middleware with authorization

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,35 +1,36 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
+const ErrorResponse = require('../utils/ErrorResponse');
 
+// Protect routes - user must be logged in
 exports.protect = async (req, res, next) => {
   let token;
 
-  if (
-    req.headers.authorization &&
-    req.headers.authorization.startsWith('Bearer')
-  ) {
+  if (req.headers.authorization?.startsWith('Bearer')) {
     token = req.headers.authorization.split(' ')[1];
   }
 
   if (!token) {
-    return res.status(401).json({
-      success: false,
-      error: 'Not authorized to access this route'
-    });
+    return next(new ErrorResponse('Not authorized to access this route', 401));
   }
 
   try {
-    // Verify token
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    
-    // Add user to request object
     req.user = await User.findById(decoded.id);
-    
     next();
   } catch (err) {
-    return res.status(401).json({
-      success: false,
-      error: 'Not authorized to access this route'
-    });
+    return next(new ErrorResponse('Not authorized to access this route', 401));
   }
+};
+
+// Authorize specific roles - must be used after protect
+exports.authorize = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new ErrorResponse(`User role ${req.user.role} is not authorized`, 403)
+      );
+    }
+    next();
+  };
 };
