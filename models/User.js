f6117daf11c3b5a374e2ecad400a7cee9c1143e4
samueloguta <ogutasamuel367@gmail.com>
2025-07-20 15:05:11 +0300

    Fix duplicate schema declaration

--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
+// SINGLE Schema declaration with all fields
 const UserSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -21,30 +22,30 @@ const UserSchema = new mongoose.Schema({
     minlength: 6,
     select: false
   },
+  role: {
+    type: String,
+    enum: ['user', 'admin'],
+    default: 'user'
+  },
   createdAt: {
     type: Date,
     default: Date.now
   }
 });
 
-// Encrypt password before saving
+// Middleware
 UserSchema.pre('save', async function(next) {
   if (!this.isModified('password')) return next();
+  
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
+  next();
 });
 
-// Method to compare entered password with hashed password
+// Method
 UserSchema.methods.matchPassword = async function(enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
 
+// Single export
 module.exports = mongoose.model('User', UserSchema);
-const UserSchema = new mongoose.Schema({
-  // ... existing fields ...
-  role: {
-    type: String,
-    enum: ['user', 'admin'],
-    default: 'user'
-  }
-});
