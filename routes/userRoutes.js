141f52825e6d6f73c778b6b57a1b1126a410018d
samueloguta <ogutasamuel367@gmail.com>
2025-07-20 15:46:58 +0300

    Add complete user routes file

---
+++ routes/userRoutes.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+const { protect, authorize } = require('../middleware/auth');
+const {
+  getUsers,
+  getUser,
+  updateUser,
+  deleteUser
+} = require('../controllers/userController');
+
+// Admin-only routes
+router.use(protect);
+router.use(authorize('admin'));
+
+router.route('/')
+  .get(getUsers);
+
+router.route('/:id')
+  .get(getUser)
+  .put(updateUser)
+  .delete(deleteUser);
+
+module.exports = router;
